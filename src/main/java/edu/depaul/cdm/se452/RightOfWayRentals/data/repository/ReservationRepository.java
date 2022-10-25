696f7ca5d538fd89bb2b2b0d5ad2b62c63304d09
dedovicnermin <dedovicnermin@outlook.com>
2022-10-24 22:12:20 -0500

    ROWR : update reservation repo

--- src/main/java/edu/depaul/cdm/se452/RightOfWayRentals/data/repository/ReservationRepository.java
+++ src/main/java/edu/depaul/cdm/se452/RightOfWayRentals/data/repository/ReservationRepository.java
@@ -1,6 +1,8 @@
 package edu.depaul.cdm.se452.RightOfWayRentals.data.repository;
 
+import edu.depaul.cdm.se452.RightOfWayRentals.data.model.Customer;
 import edu.depaul.cdm.se452.RightOfWayRentals.data.model.Reservation;
+import edu.depaul.cdm.se452.RightOfWayRentals.data.model.Vehicle;
 import edu.depaul.cdm.se452.RightOfWayRentals.data.pojo.ReservationStatus;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,7 @@ import java.util.List;
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
     List<Reservation> findAllByStatus(ReservationStatus reservationStatus);
+    List<Reservation> findAllByVehicleAndCustomer(Vehicle vehicle, Customer customer);
+    List<Reservation> findAllByVehicle(Vehicle vehicle);
+    List<Reservation> findAllByCustomer(Customer customer);
 }
