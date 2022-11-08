394b5a6acc68cd36108944f4434cc4791303b34f
dedovicnermin <dedovicnermin@outlook.com>
2022-11-07 19:41:40 -0600

    ROWR : UI cancel reservation for customer with session

--- src/main/java/edu/depaul/cdm/se452/rightOfWayRentals/controller/RentalCarUIController.java
+++ src/main/java/edu/depaul/cdm/se452/rightOfWayRentals/controller/RentalCarUIController.java
@@ -2,6 +2,7 @@ package edu.depaul.cdm.se452.rightOfWayRentals.controller;
 
 import edu.depaul.cdm.se452.rightOfWayRentals.component.SecurityHelper;
 import edu.depaul.cdm.se452.rightOfWayRentals.data.model.Reservation;
+import edu.depaul.cdm.se452.rightOfWayRentals.data.model.Vehicle;
 import edu.depaul.cdm.se452.rightOfWayRentals.data.pojo.request.PostReservationRequest;
 import edu.depaul.cdm.se452.rightOfWayRentals.security.CustomerUserDetails;
 import edu.depaul.cdm.se452.rightOfWayRentals.service.CustomerService;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -51,25 +54,39 @@ public class RentalCarUIController {
 
     @GetMapping("/vehicles/all")
     public String getVehicles(final Model model) {
-        model.addAttribute("vehicles", vehicleService.getAllVehicles());
+        log.trace("Retrieving all vehicles to display on UI");
+        final List<Vehicle> allVehicles = vehicleService.getAllVehicles();
+        model.addAttribute("vehicles", allVehicles);
+        log.trace("Returning vehicles view with {} vehicles listed", allVehicles.size());
         return "vehicles";
     }
 
     @GetMapping("/vehicles/reserve/{id}")
     public String reserveVehicleWithId(@PathVariable final Long id, final Model model) {
+        log.trace("Retrieving data required for reserve vehicle view with vehicle ID {}", id);
         final PostReservationRequest formWrapper = new PostReservationRequest();
         formWrapper.setVehicleId(id);
         model.addAttribute("reservationRequest", formWrapper);
         model.addAttribute("vehicleId", id);
+        log.trace("Returning reserve vehicle view with reservation request DAO attached to model.");
         return "reserve-vehicle";
     }
 
     @PostMapping("/vehicles/reserve")
     public String postReservationForVehicle(@ModelAttribute PostReservationRequest reservationRequest, final Model model) {
-        log.info(reservationRequest.toString());
+        log.trace("Received request to reserve vehicle with ID : {}",reservationRequest.getVehicleId());
         reservationRequest.setCustomerId(securityHelper.getCurrentCustomer().getCustomerId());
         final Reservation reservation = reservationService.addReservation(reservationRequest);
+        model.addAttribute("message", "Successfully created reservation : " + reservation.toString());
+        log.trace("Reservation for vehicle ID {} successful", reservationRequest.getVehicleId());
+        return "success";
+    }
 
+    @GetMapping("/reservations/{id}/cancel")
+    public String cancelReservation(@PathVariable final Long id, final Model model) {
+        log.trace("Beginning to cancel reservation with ID : {}", id);
+        final Reservation reservation = reservationService.removeReservation(id);
+        model.addAttribute("message", "Reservation with ID : " + id + " was successfully removed. Referenced reservation : " + reservation);
         return "success";
     }
 
